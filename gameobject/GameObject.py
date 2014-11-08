e13258cfd3cf3adf2d718551b9ee4f400079c14b
Susan Lunn <slgems@gmail.com>
2014-11-08 16:19:28 -0500

    munchables class, player class, enemies class all added to gameobject

--- gameobject/GameObject.py
+++ gameobject/GameObject.py
@@ -1,9 +1,49 @@
+#player class
 class Player:
-    __tablename__ = 'player'
 
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.points = 0
+
+    def position(self):
+        return self.x, self.y
+
+    def points(self):
+        return self.points
+
+    def update(self, x, y, points=0):
+        self.x = x
+        self.y = y
+        self.points+= points
+
+#list of enemies
 class Enemies:
-    __tablename__ = 'enemies'
 
+    PIRATE = 1
+    DINOSAUR = 2
+
+    def __init__(self):
+        self.type = self.PIRATE
+        self.x = 0
+        self.y = 0
+
+    def update(self, x, y):
+        self.x = x
+        self.y = y
+
+    def type(self):
+        return self.type
+
+
+#available munchables
 class Munchables:
-    __tablename__ = 'munchables'
+    #put the types of munchables here
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        return self.x, self.y
+
+
 
